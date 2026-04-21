d9bf5f426a957165c96c98c908e288e6585240e4
Twoje Imię Nazwisko <twoj@email.com>
2026-04-21 09:00:22 +0200

    feat: dodano logige JS i funkcje dodawania

---
+++ app.js
@@ -0,0 +1,22 @@
+const display = document.getElementById('display');
+
+// Dodaje znaki do wyświetlacza
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+// Czyści wyświetlacz (akcja dla przycisku C)
+function clearDisplay() {
+    display.value = "";
+}
+
+// Oblicza wynik
+function calculate() {
+    try {
+        // eval() wykonuje tekst jako działanie matematyczne
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Błąd";
+        setTimeout(clearDisplay, 1500);
+    }
+}
